7b2d8c4fc10e7f677266e9e856ed285429393725
Yato Aki <yatoakii@gmail.com>
2022-03-31 16:54:31 +0630

    Add Input Taking with Buttons

--- script.js
+++ script.js
@@ -25,3 +25,55 @@ function operator(a,b,sign) {
     return div(a,b)
   }
 }
+
+let firstNumber;
+let secondNumber;
+let currentNumber;
+let currentOperator;
+
+function updateCurrentResult(a) {
+  currentResult = a;
+}
+
+function updateFirstNumber(a) {
+  firstNumber = a;
+}
+
+function updateSecondNumber(a) {
+  secondNumber = a;
+}
+
+function updateCurrentOperator(a) {
+  currentOperator = a;
+}
+
+function updateCurrentNumber(a) {
+  if (!currentNumber){
+    currentNumber = a;
+  }else{
+    currentNumber += a;
+  }
+}
+
+const operatorBtns = document.querySelectorAll(".operators")
+const numberBtns = document.querySelectorAll(".numbers")
+
+numberBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    updateCurrentNumber(e.target.innerHTML);
+  });
+});
+
+operatorBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    updateCurrentResult(e.target.innerHTML);
+    if (!firstNumber){
+      updateFirstNumber(currentNumber);
+      console.log(firstNumber);
+    }else{
+      updateSecondNumber(currentNumber);
+      console.log(secondNumber);
+    }
+    currentNumber = undefined;
+  });
+});
